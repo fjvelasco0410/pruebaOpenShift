d08aab760b18b844e9db8d311474aa3e03fde65a
fjvelasco <fjv0410@gmail.com>
2019-06-26 18:03:17 -0500

    hola mundo

--- src/main/java/com/fjvelasco/controller/PruebaController.java
+++ src/main/java/com/fjvelasco/controller/PruebaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PruebaController {
-	@RequestMapping("/")
+	@RequestMapping("/hola")
 	public String hello(){
 		return "hello";
 	}
