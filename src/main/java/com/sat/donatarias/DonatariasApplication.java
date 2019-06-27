6e12f8cbee4d20fb19bcbe3db9e2c37d06484f0e
fjvelasco <fjv0410@gmail.com>
2019-06-27 09:36:03 -0500

    cambia controller

---
+++ src/main/java/com/sat/donatarias/DonatariasApplication.java
@@ -0,0 +1,17 @@
+package com.sat.donatarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DonatariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatariasApplication.class, args);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/sat/donatarias/controller/BandejaSeguimientoAsuntosController.java
@@ -0,0 +1,60 @@
+package com.sat.donatarias.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sat.donatarias.bean.SolicitudBean;
+import com.sat.donatarias.bean.TablaBusquedaAtenderSolicitudAbogadoBean;
+import com.sat.donatarias.bean.TipoSolicitudBean;
+import com.sat.donatarias.model.EstadoProcesal;
+import com.sat.donatarias.model.TipoSolicitud;
+import com.sat.donatarias.service.AtencionSolicitudesAbogadoService;
+import com.sat.donatarias.service.EstadoProcesalService;
+import com.sat.donatarias.service.TipoSolicitudService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/bandejaSeguimientoAsuntos")
+public class BandejaSeguimientoAsuntosController {
+
+	@Autowired
+	TipoSolicitudService tipoSolicitudService;
+
+	@Autowired
+	EstadoProcesalService estadoProcesalService;
+	
+	@Autowired
+	AtencionSolicitudesAbogadoService atencionSolicitudesAbogadoService;
+	
+	@GetMapping("/obtenListaTipoSolicitud")
+	public List<TipoSolicitud> obtenListaTipoSolicitud() {
+		List<TipoSolicitud> lstTipoSolicitud = tipoSolicitudService.obtenListaTipoSolicitud();
+		return lstTipoSolicitud;
+	}
+
+	@GetMapping("/obtenListaEstadoProcesal")
+	public List<EstadoProcesal> obtenListaEstadoProcesal() {
+		List<EstadoProcesal> listaEstadoProcesal = estadoProcesalService.obtenListaEstadoProcesal();
+		return listaEstadoProcesal;
+	}
+
+	@PostMapping("/buscar")
+	public List<TablaBusquedaAtenderSolicitudAbogadoBean> buscar(@RequestBody TipoSolicitudBean tipoSolicitudBean) {
+		List<TablaBusquedaAtenderSolicitudAbogadoBean> lista = atencionSolicitudesAbogadoService.buscar(tipoSolicitudBean);
+		return lista;
+	}
+
+	@PostMapping("/consultarSolicitud")
+	public SolicitudBean consultarSolicitud(@RequestBody TablaBusquedaAtenderSolicitudAbogadoBean tablaBusquedaAtenderSolicitudAbogado) {
+		SolicitudBean solicitudBean = atencionSolicitudesAbogadoService.consultarDatosSolicitud(tablaBusquedaAtenderSolicitudAbogado);
+		return solicitudBean;
+	}
+				
+}
